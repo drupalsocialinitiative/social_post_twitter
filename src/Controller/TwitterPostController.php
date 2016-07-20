aa84304d4a3bc5485fb63348281ac1433bdb7c73
Getulio Sánchez <valentin2507@gmail.com>
2016-07-20 13:28:38 -0400

    Authorize app in twitter and get the access token

--- src/Controller/TwitterPostController.php
+++ src/Controller/TwitterPostController.php
@@ -2,12 +2,99 @@
 
 namespace Drupal\social_post_twitter\Controller;
 
+use Abraham\TwitterOAuth\TwitterOAuth;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\social_api\Plugin\NetworkManager;
+use Drupal\social_post_twitter\Plugin\Network\TwitterPost;
+use Drupal\social_post_twitter\TwitterPostManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Zend\Diactoros\Response\RedirectResponse;
+
 /**
  * Manages requests to Twitter
  */
-class TwitterPostController {
+class TwitterPostController extends ControllerBase {
 
-  public function redirectToGoogle() {
+  /**
+   * The network plugin manager.
+   *
+   * @var \Drupal\social_api\Plugin\NetworkManager
+   */
+  protected $networkManager;
 
+  /**
+   * The twitter post manager.
+   *
+   * @var \Drupal\social_post_twitter\TwitterPostManager
+   */
+  protected $twitterManager;
+
+  /**
+   * TwitterPostController constructor.
+   *
+   * @param \Drupal\social_api\Plugin\NetworkManager $network_manager
+   *   The network plugin manager.
+   * @param \Drupal\social_post_twitter\TwitterPostManager $twitter_manager
+   *   The Twitter post manager.
+   */
+  public function __construct(NetworkManager $network_manager, TwitterPostManager $twitter_manager) {
+    $this->networkManager = $network_manager;
+    $this->twitterManager = $twitter_manager;
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('plugin.network.manager'),
+      $container->get('twitter_post.manager')
+    );
+  }
+
+  /**
+   * Redirects user to Twitter for authentication.
+   *
+   * @return \Zend\Diactoros\Response\RedirectResponse
+   *   Redirects to Twitter.
+   *
+   * @throws \Abraham\TwitterOAuth\TwitterOAuthException
+   */
+  public function redirectToTwitter() {
+    /* @var TwitterPost $network_plugin */
+    $network_plugin = $this->networkManager->createInstance('social_post_twitter');
+
+    /* @var TwitterOAuth $connection */
+    $connection = $network_plugin->getSdk();
+
+    $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => $network_plugin->getOauthCallback()));
+
+    // Saves the request token values in session.
+    $this->twitterManager->setOauthToken($request_token['oauth_token']);
+    $this->twitterManager->setOauthTokenSecret($request_token['oauth_token_secret']);
+
+    // Generates url for authentication.
+    $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+
+    return new RedirectResponse($url);
+  }
+
+  /**
+   * Callback function for the authentication process.
+   *
+   * @throws \Abraham\TwitterOAuth\TwitterOAuthException
+   */
+  public function callback() {
+    $oauth_token = $this->twitterManager->getOauthToken();
+    $oauth_token_secret = $this->twitterManager->getOauthTokenSecret();
+
+    /* @var TwitterOAuth $connection */
+    $connection = $this->networkManager->createInstance('social_post_twitter')->getSdk2($oauth_token, $oauth_token_secret);
+
+    // Gets the permanent access token.
+    $access_token = $connection->oauth('oauth/access_token', array('oauth_verifier' => $this->twitterManager->getOauthVerifier()));
+
+    var_dump($access_token);
+  }
+
 }
--- src/Plugin/Network/TwitterPost.php
+++ src/Plugin/Network/TwitterPost.php
@@ -4,8 +4,13 @@ namespace Drupal\social_post_twitter\Plugin\Network;
 
 
 use Abraham\TwitterOAuth\TwitterOAuth;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Render\MetadataBubblingUrlGenerator;
 use Drupal\social_api\SocialApiException;
 use Drupal\social_post\Plugin\Network\SocialPostNetwork;
+use Drupal\social_post_twitter\TwitterPostManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines Social Post Twitter Network Plugin.
@@ -23,6 +28,60 @@ use Drupal\social_post\Plugin\Network\SocialPostNetwork;
  * )
  */
 class TwitterPost extends SocialPostNetwork {
+
+  /**
+   * The url generator.
+   *
+   * @var \Drupal\Core\Render\MetadataBubblingUrlGenerator
+   */
+  protected $urlGenerator;
+
+  /**
+   * The Twitter manager.
+   *
+   * @var \Drupal\social_post_twitter\TwitterPostManager
+   */
+  private $twitterManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $container->get('url_generator'),
+      $container->get('twitter_post.manager'),
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * TwitterPost constructor.
+   *
+   * @param \Drupal\Core\Render\MetadataBubblingUrlGenerator $url_generator
+   *   Used to generate a absolute url for authentication.
+   * @param \Drupal\social_post_twitter\TwitterPostManager $twitter_manager
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   */
+  public function __construct(MetadataBubblingUrlGenerator $url_generator, TwitterPostManager $twitter_manager, array $configuration, $plugin_id, $plugin_definition,
+                              EntityTypeManagerInterface $entity_type_manager, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $config_factory);
+
+    $this->urlGenerator = $url_generator;
+    $this->twitterManager = $twitter_manager;
+  }
 
   /**
    * {@inheritdoc}
@@ -44,6 +103,38 @@ class TwitterPost extends SocialPostNetwork {
    */
   public function doPost() {
     // TODO: Implement doPost() method.
+  }
+
+  /**
+   * Gets the absolute url of the callback.
+   *
+   * @return string.
+   *   The callback url.
+   */
+  public function getOauthCallback() {
+    return $this->urlGenerator->generateFromRoute('social_post_twitter.callback', array(), array('absolute' => TRUE));
+  }
+
+  /**
+   * Gets a TwitterOAuth token with oauth_token and oauth_token_secret.
+   * This method is used in the callback method when Twitter returns a
+   * temporary token and token secret which should be used to get the
+   * permanent access token and access token secret.
+   *
+   * @param string $oauth_token
+   *   The oauth token.
+   * @param $oauth_token_secret
+   *   The oauth token secret.
+   *
+   * @return \Abraham\TwitterOAuth\TwitterOAuth
+   *   The twitter oauth client.
+   */
+  public function getSdk2($oauth_token, $oauth_token_secret) {
+    /* @var \Drupal\social_post_twitter\Settings\TwitterPostSettings $settings */
+    $settings = $this->settings;
+
+    return new TwitterOAuth($settings->getConsumerKey(), $settings->getConsumerSecret(),
+                $oauth_token, $oauth_token_secret);
   }
 
 }
